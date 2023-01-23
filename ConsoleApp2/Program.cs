06e29df1fb5e79c292bf0f99c611b917fa03d29e
Егор Петров <egor2007002@gmail.com>
2023-01-23 15:30:34 +0300

    matrixB

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        
         }
+        public void MatrixB(int length, int width)
+        {
+            Random random = new Random();
+            random.Next(0, 2);
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    
+                    Console.WriteLine(random);
+                    random.Next(0,2);
+                }
+            }
+        }   
     }
 }
